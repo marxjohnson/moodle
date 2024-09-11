9fa485c038ca9fcbece2709ffaad89bbfa0faf21
Mark Johnson <mark.johnson@catalyst-eu.net>
2024-09-11 15:20:07 +0100

    fixup! MDL-81514 assign: Enable filtering by non-participation groups

--- mod/assign/classes/output/grading_actionmenu.php
+++ mod/assign/classes/output/grading_actionmenu.php
@@ -108,7 +108,7 @@ class grading_actionmenu implements templatable, renderable {
 
         if (groups_get_activity_groupmode($cm, $course)) {
             $data['groupselector'] = $actionbarrenderer->render(
-                new \core_course\output\actionbar\group_selector(null, $PAGE->context));
+                new \core_course\output\actionbar\group_selector(null, $PAGE->context, participationonly: false));
         }
 
         if ($extrafiltersdropdown = $this->get_extra_filters_dropdown()) {
