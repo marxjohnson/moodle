6a35cc49b26f0a7b6254a905c4f4b2f07c81f0dc
Mark Johnson <mark.johnson@catalyst-eu.net>
2024-12-04 13:18:30 +0000

    MDL-83883 qbank_editquestion: Add status filter condition
    This allows users to filter questions based on whether a question is in
    "draft" or "ready" status.

--- question/bank/editquestion/lang/en/qbank_editquestion.php
+++ question/bank/editquestion/lang/en/qbank_editquestion.php
@@ -23,6 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['invalidstatusfilter'] = 'Invalid value "{$a}" for status filter.';
+
 $string['pluginname'] = 'Edit questions';
 $string['privacy:metadata'] = 'The Edit questions question bank plugin does not store any personal data.';
 
