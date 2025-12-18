da9ab65cf8f41a382f3989781561288d31d66f41
Mark Johnson <mark.johnson@catalyst-eu.net>
2025-12-18 08:44:34 +0000

    MDL-87103 qbank_viewquestionname: Update question name cell
    This replaces the `display_content` method of the
    `question_name_idnumber_tags_column` class with
    a `render` method, to return the HTML for a
    question name cell using a template,
    rather than echoing it directly.

--- public/question/bank/viewquestionname/classes/viewquestionname_column_helper.php
+++ public/question/bank/viewquestionname/classes/viewquestionname_column_helper.php
@@ -16,6 +16,7 @@
 
 namespace qbank_viewquestionname;
 
+use core\attribute\deprecated;
 use core_question\local\bank\column_base;
 
 
@@ -53,6 +54,13 @@ class viewquestionname_column_helper extends column_base {
         return '';
     }
 
+    #[deprecated(
+        replacement: column_base::class . '::render',
+        since: 5.2,
+        reason: 'Direct output of HTML was replaced with functions to return the rendered HTML for display',
+        mdl: 'MDL-87103',
+        emit: false,
+    )]
     protected function display_content($question, $rowclasses): void {
         $labelfor = $this->label_for($question);
         if ($labelfor) {
