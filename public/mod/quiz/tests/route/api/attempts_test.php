b0218326198c08a29f0df2dbb7ac7045fb0376d9
Mark Johnson <mark.johnson@catalyst-eu.net>
2026-08-25 15:51:17 +0100

    fixup! MDL-82654 quiz: Retain backwards compatibility in web services
    Expand unit tests

--- public/mod/quiz/tests/route/api/attempts_test.php
+++ public/mod/quiz/tests/route/api/attempts_test.php
@@ -16,8 +16,10 @@
 
 namespace mod_quiz\route\api;
 
+use core\context\course;
 use core\router\route_loader_interface;
 use core\tests\router\route_testcase;
+use mod_quiz\output\grades\grade_out_of;
 use mod_quiz\quiz_attempt;
 use mod_quiz\tests\attempt_helper_test_trait;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -44,6 +46,8 @@ final class attempts_test extends route_testcase {
             attempts::class,
             route_loader_interface::ROUTE_GROUP_API,
         );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
 
         // Create a new quiz with two questions and one attempt finished.
         $generator = $this->getDataGenerator();
@@ -80,6 +84,7 @@ final class attempts_test extends route_testcase {
         $this->assertEquals(50, $review['grade']);
         $this->assertEquals(1, $review['attempt']['attempt']);
         $this->assertEquals('finished', $review['attempt']['state']);
+        $this->assertEqualsWithDelta(time(), $review['attempt']['gradednotificationsenttime'], 1.0);
         $this->assertEquals(1, $review['attempt']['sumgrades']);
         $this->assertCount(2, $review['questions']);
         $this->assertEquals('gradedright', $review['questions'][0]['state']);
@@ -94,8 +99,145 @@ final class attempts_test extends route_testcase {
         $this->assertEquals('Feedback text 1', $review['additionaldata'][0]['content']);
 
         // Additional grades.
-        $this->assertEquals(['name' => 'Listening', 'grade' => 1, 'maxgrade' => 1], $review['attempt']['gradeitemmarks'][0]);
-        $this->assertEquals(['name' => 'Reading', 'grade' => 0, 'maxgrade' => 1], $review['attempt']['gradeitemmarks'][1]);
+        $this->assertEquals(
+            json_decode(json_encode(new grade_out_of($attemptobj->get_quiz(), 1.0, 1.0, 'Listening')), true),
+            $review['attempt']['gradeitemmarks'][0],
+        );
+        $this->assertEquals(
+            json_decode(json_encode(new grade_out_of($attemptobj->get_quiz(), 0, 1.0, 'Reading')), true),
+            $review['attempt']['gradeitemmarks'][1],
+        );
+    }
+
+    /**
+     * Getting a review in submitted state succeeds, with null grades and feedback.
+     *
+     * @return void
+     */
+    public function test_get_review_submitted(): void {
+        global $DB;
+        $this->resetAfterTest();
+        $this->add_class_routes_to_route_loader(
+            attempts::class,
+            route_loader_interface::ROUTE_GROUP_API,
+        );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
+
+        // Create a new quiz with two questions and one attempt submitted.
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course();
+        $student = $generator->create_user();
+        $generator->enrol_user($student->id, $course->id, 'student');
+        $this->setUser($student);
+        [$quiz, , $quizobj] = $this->create_quiz_with_questions($course->id, $student->id);
+        [, $attemptobj] = $this->create_quiz_attempt_object($quizobj, $student->id);
+        $this->answer_attempt($attemptobj, [1 => ['answer' => 1]]);
+        $attemptobj->process_submit(time(), true);
+
+        // Add feedback.
+        $feedback = new \stdClass();
+        $feedback->quizid = $quiz->id;
+        $feedback->feedbacktext = 'Feedback text 1';
+        $feedback->feedbacktextformat = 1;
+        $feedback->mingrade = 49;
+        $feedback->maxgrade = 100;
+        $feedback->id = $DB->insert_record('quiz_feedback', $feedback);
+
+        // Add some extra grade items.
+        $quizgenerator = $this->getDataGenerator()->get_plugin_generator('mod_quiz');
+        $listeninggrade = $quizgenerator->create_grade_item(['quizid' => $attemptobj->get_quizid(), 'name' => 'Listening']);
+        $readinggrade = $quizgenerator->create_grade_item(['quizid' => $attemptobj->get_quizid(), 'name' => 'Reading']);
+        $structure = $attemptobj->get_quizobj()->get_structure();
+        $structure->update_slot_grade_item($structure->get_slot_by_number(1), $listeninggrade->id);
+        $structure->update_slot_grade_item($structure->get_slot_by_number(2), $readinggrade->id);
+        // Reload the attempt object with the new grade items.
+        $attemptobj = quiz_attempt::create($attemptobj->get_attemptid());
+
+        $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review");
+        $this->assert_valid_response($response);
+        $review = $this->decode_response($response, true);
+
+        // Two questions, one completed but ungraded, the other not complete.
+        $this->assertNull($review['grade']);
+        $this->assertEquals(1, $review['attempt']['attempt']);
+        $this->assertEquals('submitted', $review['attempt']['state']);
+        $this->assertNull($review['attempt']['gradednotificationsenttime']);
+        $this->assertNull($review['attempt']['sumgrades']);
+        $this->assertCount(2, $review['questions']);
+        $this->assertEquals('complete', $review['questions'][0]['state']);
+        $this->assertEquals(1, $review['questions'][0]['slot']);
+        $this->assertEquals('todo', $review['questions'][1]['state']);
+        $this->assertEquals(2, $review['questions'][1]['slot']);
+
+        // No feedback.
+        $this->assertEmpty($review['additionaldata']);
+
+        // No additional grades.
+        $this->assertEmpty($review['attempt']['gradeitemmarks']);
+    }
+
+    /**
+     * Getting a review in submitted state succeeds, with null grades and feedback.
+     *
+     * @return void
+     */
+    public function test_get_review_paged(): void {
+        $this->resetAfterTest();
+        $this->add_class_routes_to_route_loader(
+            attempts::class,
+            route_loader_interface::ROUTE_GROUP_API,
+        );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
+
+        // Create a new quiz with two questions on the first page, one on the second, and one attempt finished.
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course();
+        $student = $generator->create_user();
+        $generator->enrol_user($student->id, $course->id, 'student');
+        $this->setUser($student);
+        [$quiz, , $quizobj] = $this->create_quiz_with_questions($course->id, $student->id);
+        $questiongenerator = $this->getDataGenerator()->get_plugin_generator('core_question');
+        $cat = $questiongenerator->create_question_category();
+        $question = $questiongenerator->create_question('numerical', null, ['category' => $cat->id]);
+        quiz_add_quiz_question($question->id, $quiz, 1);
+        [, $attemptobj] = $this->create_quiz_attempt_object($quizobj, $student->id);
+        $this->answer_attempt($attemptobj, [1 => ['answer' => 1], 2 => ['answer' => 1], 3 => ['answer' => 1]], true);
+
+        $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review");
+        $this->assert_valid_response($response);
+        $review = $this->decode_response($response, true);
+
+        // No page parameter, we get all questions.
+        $this->assertEquals(1, $review['attempt']['attempt']);
+        $this->assertEquals('finished', $review['attempt']['state']);
+        $this->assertCount(3, $review['questions']);
+        $this->assertEquals(1, $review['questions'][0]['slot']);
+        $this->assertEquals(2, $review['questions'][1]['slot']);
+        $this->assertEquals(3, $review['questions'][2]['slot']);
+
+        $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review?page=0");
+        $this->assert_valid_response($response);
+        $review = $this->decode_response($response, true);
+
+        // Page 0, just the first 2 questions.
+        $this->assertEquals(1, $review['attempt']['attempt']);
+        $this->assertEquals('finished', $review['attempt']['state']);
+        $this->assertCount(2, $review['questions']);
+        $this->assertEquals(1, $review['questions'][0]['slot']);
+        $this->assertEquals(2, $review['questions'][1]['slot']);
+
+
+        $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review?page=1");
+        $this->assert_valid_response($response);
+        $review = $this->decode_response($response, true);
+
+        // Page 1, just the third question.
+        $this->assertEquals(1, $review['attempt']['attempt']);
+        $this->assertEquals('finished', $review['attempt']['state']);
+        $this->assertCount(1, $review['questions']);
+        $this->assertEquals(3, $review['questions'][0]['slot']);
     }
 
     /**
@@ -107,6 +249,8 @@ final class attempts_test extends route_testcase {
             attempts::class,
             route_loader_interface::ROUTE_GROUP_API,
         );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
 
         // Create a new quiz with two questions and one attempt finished.
         $generator = $this->getDataGenerator();
@@ -128,6 +272,8 @@ final class attempts_test extends route_testcase {
             attempts::class,
             route_loader_interface::ROUTE_GROUP_API,
         );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
 
         // Create a new quiz with two questions and one attempt finished.
         $generator = $this->getDataGenerator();
@@ -149,6 +295,8 @@ final class attempts_test extends route_testcase {
             attempts::class,
             route_loader_interface::ROUTE_GROUP_API,
         );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
 
         // Create a new quiz with two questions and one attempt finished.
         $generator = $this->getDataGenerator();
@@ -160,6 +308,37 @@ final class attempts_test extends route_testcase {
         [, , , , $attemptobj] = $this->create_quiz_with_questions($course->id, $student->id, true, true);
         // Try to review a different user's attempt.
         $this->setUser($student2);
+        $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review");
+        $this->assert_valid_response($response, 403);
+    }
+
+    /**
+     * Trying to get review your own attempt when you don't have permission to see the review returns 403 Forbidden.
+     */
+    public function test_get_review_no_capability(): void {
+        global $DB;
+        $this->resetAfterTest();
+        $this->add_class_routes_to_route_loader(
+            attempts::class,
+            route_loader_interface::ROUTE_GROUP_API,
+        );
+        // Prevent theme initialisation in quiz setup.
+        filter_set_global_state('emoticon', TEXTFILTER_OFF);
+
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course();
+        $student = $generator->create_user();
+        $generator->enrol_user($student->id, $course->id, 'student');
+        // Create a new quiz with two questions and one attempt finished.
+        [, , , , $attemptobj] = $this->create_quiz_with_questions($course->id, $student->id, true, true);
+        // Prohibit students from reviewing their own attempts.
+        $generator->create_role_capability(
+            $DB->get_field('role', 'id', ['shortname' => 'student']),
+            ['mod/quiz:attempt' => 'prohibit'],
+            course::instance($course->id),
+        );
+        // Try to review the attempt as the student.
+        $this->setUser($student);
         $response = $this->process_api_request('GET', "/attempts/{$attemptobj->get_attemptid()}/review");
         $this->assert_valid_response($response, 403);
     }
